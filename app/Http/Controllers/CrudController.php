d1a845988f14e6366487e8e2e724608d194c157e
root <root@buchet>
2025-11-03 23:14:40 +0900

    eloquent orm 작성 및 blade template 목록 노출 처리

--- app/Http/Controllers/CrudController.php
+++ app/Http/Controllers/CrudController.php
@@ -13,11 +13,11 @@ class CrudController extends Controller
         $q = trim($request->input('q', ''));
         $per = trim($request->input('per', '10'));
 
-        $list = CrudModel::when($q, function ($query, $q) {
+        $lists = CrudModel::when($q, function ($query, $q) {
             $query->where('subject', 'like', "%{$q}%")
                     ->orWhere('content', 'like', "%{$q}%");
         })->paginate($per);
 
-        return view('crud.index', compact('list', 'q', 'per'));
+        return view('crud.index', compact('lists', 'q', 'per'));
     }
 }
--- resources/views/crud/index.blade.php
+++ resources/views/crud/index.blade.php
@@ -4,6 +4,31 @@
 
 @section('content')
 <div class="md:container m-auto h-screen text-center content-center">
-    Crud index 본문 내용
+    <h1 class="text-3xl font-bold underline">
+        Crud Test Page
+    </h1>
+
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>User ID</th>
+                <th>Subject</th>
+                <th>createTime</th>
+                <th>updateTime</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($lists as $list)
+            <tr>
+                <td>{{ $list->id }}</td>
+                <td>{{ $list->user_id }}</td>
+                <td>{{ $list->subject }}</td>
+                <td>{{ $list->created_at }}</td>
+                <td>{{ $list->updated_at }}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
 </div>
 @endsection
